42218d8df82f2efc65a3c55d60ccee804d773ac3
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-05-14 18:47:12 -0600

    thought this if statement wasn't working properly before, but now it is. prepping for a deploy

--- app/index.js
+++ app/index.js
@@ -12,6 +12,6 @@ ReactDOM.render(
 
 
 // https://webpack.github.io/docs/hot-module-replacement.html
-//if (module.hot) {
+if (module.hot) {
   module.hot.accept(); // avoids "[HMR] The following modules couldn't be hot updated: (Full reload needed) This is usually because the modules which have changed (and their parents) do not know how to hot reload themselves." error
-//}
+}
